ad4f346252d08f9f237a51d374ca2c1b89818a49
Luiz Paulo "Bills <luizbills@pm.me>
2019-11-09 09:03:58 -0300

    fix print

--- main.php
+++ main.php
@@ -14,5 +14,5 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 if ( ! defined( 'WPINC' ) ) die();
 
 add_action( 'admin_notices', function () {
-  echo "<div class="notice notice-success is-dismissible"><p>Hello World from Gitpod Test Plugin</p></div>"
+  echo '<div class="notice notice-success is-dismissible"><p>Hello World from Gitpod Test Plugin</p></div>';
 } );
