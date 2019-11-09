ec3e44060937da9744ff8235531972e4d206dbe5
Luiz Paulo "Bills <luizbills@pm.me>
2019-11-09 13:52:07 -0300

    Update wp-config.php

--- conf/wp-config.php
+++ conf/wp-config.php
@@ -24,13 +24,14 @@ define( 'WP_DEBUG_DISPLAY', false );
 
 define( 'WP_MEMORY_LIMIT', '256M' );
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
-define( 'WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] );
-define( 'WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] );
+
+if ( isset( $_SERVER['HTTP_HOST'] ) ) {
+	define( 'WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] );
+	define( 'WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] );
+}
 
 // Force SSL
 $_SERVER['HTTPS'] = 'on';
-
-
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
