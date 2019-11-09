65e9dc5e2a54b8bb5d8d03c71576a56c8de1c40a
Luiz Paulo "Bills <luizbills@pm.me>
2019-11-08 23:54:33 -0300

    Update setup-wordpress.sh

--- conf/setup-wordpress.sh
+++ conf/setup-wordpress.sh
@@ -1,31 +1,42 @@
+
 # WordPress Setup Script
 function _wp_setup () {
-  TARGET=$1
+  FLAG=".wordpress-installed"
+
+  # search the flag file
+  if [ -f $FLAG ]; then
+    echo 'WordPress already installed'
+    return 1
+  fi
   
-  cd $HOME
+  REPO_NAME=$(basename $GITPOD_REPO_ROOT)
+  DESTINATION=${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-content/$1/${REPO_NAME}
   
+  # install project dependencies
+  cd ${GITPOD_REPO_ROOT}
+  if [ -f composer.json ]; then
+    composer install
+  fi
+  if [ -f package.json ]; then
+    npm install
+  fi
+
   # move the workspace temporarily
   mkdir $HOME/workspace
-  mv ${GITPOD_REPO_ROOT}/.[!.]* $HOME/workspace
-  
+  mv ${GITPOD_REPO_ROOT} $HOME/workspace
+
   # create webserver root and install WordPress there
-  mkdir ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
-  mv $HOME/wordpress/.[!.]* ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
+  mkdir -p ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
+  cp -R $HOME/wordpress/* ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
+
+  # put the project files in the correct place
+  mkdir $DESTINATION
+  mv $HOME/workspace/.[!.]* $DESTINATION
   
-  if [ -f composer.json ]; then
-    # composer install
-  fi
-  
-  if [ -f package.json ]; then
-    # npm install
-  fi
-  
-  mv $HOME/workspace/.[!.]* ${GITPOD_REPO_ROOT}/${APACHE_APACHE_DOCROOT}/wp-content/${TARGET}
-  mv $HOME/gitpod-wordpress/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_APACHE_DOCROOT}/wp-config.php
-  
-  cd /workspace/${APACHE_APACHE_DOCROOT}
-  
-  apacheclt start
+  # create a wp-config.php
+  cp $HOME/gitpod-wordpress/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
+
+  cd $DESTINATION
 }
 
 function wp_setup_theme () {
