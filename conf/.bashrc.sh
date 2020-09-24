d6a3f50d88969017e41bbc44c3ea17b4c8af32bc
Luiz "Bills <luizpbills@gmail.com>
2020-09-24 13:53:05 -0300

    Update .bashrc.sh

--- conf/.bashrc.sh
+++ conf/.bashrc.sh
@@ -20,27 +20,39 @@ function wp-setup () {
     echo 'WordPress already installed'
     return 1
   fi
-  
-  DESTINATION=${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-content/$1/${REPO_NAME}
 
   echo 'Please, wait ...'
 
   # this would cause mv below to match hidden files
   shopt -s dotglob
   
-  echo 'Creating MySQL user and database ...'
-  wp-init-database 1> /dev/null
-
   # move the workspace temporarily
   mkdir $HOME/workspace
   mv ${GITPOD_REPO_ROOT}/* $HOME/workspace/
-
-  echo 'Downloading WordPress ...'
-  wget -q https://wordpress.org/latest.zip -O $HOME/wordpress.zip
-  unzip -qn $HOME/wordpress.zip -d $HOME
-  unlink $HOME/wordpress.zip
-  cp $HOME/gitpod-wordpress/conf/.htaccess $HOME/wordpress/.htaccess
   
+  # create a debugger launch.json
+  mkdir -p ${GITPOD_REPO_ROOT}/.theia
+  mv $HOME/gitpod-wordpress/conf/launch.json ${GITPOD_REPO_ROOT}/.theia/launch.json
+  
+  # create a database for this WordPress
+  echo 'Creating MySQL user and database ...'
+  wp-init-database 1> /dev/null
+  
+  echo 'Installing WordPress ...'
+  mkdir -p ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
+  mv $HOME/wordpress/* ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
+  
+  # install WordPress
+  cd ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
+  wp core download
+  cp $HOME/gitpod-wordpress/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
+  wp core install \
+    --url="$(gp url 8080 | sed -e s/https:\\/\\/// | sed -e s/\\///)" \
+    --title="WordPress" \
+    --admin_user="admin" \
+    --admin_password="password" \
+    --admin_email="admin@gitpod.test"
+    
   echo 'Downloading Adminer ...'
   mkdir $HOME/wordpress/database/
   wget -q https://www.adminer.org/latest.php -O $HOME/wordpress/database/index.php
@@ -48,32 +60,12 @@ function wp-setup () {
   echo 'Creating phpinfo() page ...'
   mkdir $HOME/wordpress/phpinfo/
   echo "<?php phpinfo(); ?>" > $HOME/wordpress/phpinfo/index.php
-  
-  # create webserver root and install WordPress there
-  echo 'Installing WordPress ...'
-  mkdir -p ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}
-  mv $HOME/wordpress/* ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
 
   # put the project files in the correct place
-  mkdir $DESTINATION
-  mv $HOME/workspace/* $DESTINATION
-  
-  # create a wp-config.php
-  cp $HOME/gitpod-wordpress/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
-  
-  # create a debugger launch.json
-  mkdir ${GITPOD_REPO_ROOT}/.theia
-  mv $HOME/gitpod-wordpress/conf/launch.json ${GITPOD_REPO_ROOT}/.theia/launch.json
-
-  # Setup WordPress database
-  cd ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/
-  wp core install \
-    --url="$(gp url 8080 | sed -e s/https:\\/\\/// | sed -e s/\\///)" \
-    --title="WordPress" \
-    --admin_user="admin" \
-    --admin_password="password" \
-    --admin_email="admin@gitpod.test"
-
+  echo 'Creating project files ...'
+  PROJECT_PATH=${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-content/$1/${REPO_NAME}
+  mkdir -p $PROJECT_PATH
+  mv $HOME/workspace/* ${PROJECT_PATH}
   cd $DESTINATION
 
   # install project dependencies
