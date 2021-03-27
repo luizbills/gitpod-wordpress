51eeb3cc413c3a1291e9689c24ccee5835141b96
Luiz "Bills <luizbills@pm.me>
2021-03-27 00:01:05 -0300

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -1,5 +1,7 @@
 # docs: https://developer.wordpress.org/cli/commands/plugin/
 
+wp rewrite structure '/%year%/%monthnum%/%postname%/'
+
 wp plugin delete hello
 wp plugin delete akismet
 wp plugin activate ${REPO_NAME}
